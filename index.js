16689e42f7fb6a60df83975373efd851a359177f
Mahed21 <mahed255261@gmail.com>
2021-12-21 14:16:57 +0600

    second commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = "mongodb+srv://easy-parcel:jOfoLniqK0iG9LrH@cluster0.pszjp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pszjp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 async function run() {
   try {
